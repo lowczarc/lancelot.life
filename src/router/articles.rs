636c96899c397cfd7f7e2eaab2b23bca257b442a
Lancelot OWCZARCZAK <lowczarc@student.42.fr>
2019-05-21 00:06:20 +0200

    feat: modify article

--- src/router/articles.rs
+++ src/router/articles.rs
@@ -22,10 +22,17 @@ pub fn super_route(req: Request) -> Response {
     let title = ViewVar::Simple("Super titre".into());
     vars.insert("title".into(), &title);
 
-    let name = ViewVar::Simple(req.query);
-    vars.insert("query".into(), &name);
+    let title1 = ViewVar::Simple("<h1>Super titre</h1>".into());
+    let title2 = ViewVar::Simple("<h2>Deuxieme titre</h2>".into());
+    let title3 = ViewVar::Simple("<h3>Troisième titre</h3>".into());
+    let title4 = ViewVar::Simple("<h4>Quatrième titre</h4>".into());
+    let titres = ViewVar::Array(vec![title1, title2, title3, title4]);
+    vars.insert("titres".into(), &titres);
 
-    res.header("Content-Type".into(), "text/html".into());
+    let query = ViewVar::Simple(req.query);
+    vars.insert("query".into(), &query);
+
+    res.header("Content-Type".into(), "text/html; charset=utf8".into());
     res.body(render_view(HTML_STRUCTURE, vars));
     res
 }
