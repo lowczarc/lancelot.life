8420330f1363329af96da1ae39a5d7019ee3787c
Lancelot OWCZARCZAK <lowczarc@student.42.fr>
2019-05-20 23:58:00 +0200

    feat: render dynamic views

--- src/router/articles.rs
+++ src/router/articles.rs
@@ -1,11 +1,13 @@
+use std::collections::HashMap;
+
+use lazy_static::lazy_static;
+
 use crate::{
     request::Request,
     response::Response,
     router::{Regex, Route},
-    views::HtmlView,
+    views::{HtmlView, ViewVar, render_view},
 };
-
-use lazy_static::lazy_static;
 
 const HTML_STRUCTURE: HtmlView = import_view!("views/article.html");
 
@@ -13,10 +15,17 @@ lazy_static! {
     pub static ref ARTICLES: Route = (Regex::new(r"/blabla/.*").unwrap(), super_route);
 }
 
-pub fn super_route(_req: Request) -> Response {
+pub fn super_route(req: Request) -> Response {
     let mut res = Response::new();
+    let mut vars: HashMap<String, &ViewVar> = HashMap::new();
+    
+    let title = ViewVar::Simple("Super titre".into());
+    vars.insert("title".into(), &title);
 
-    res.header("Content-Type".into(), "text/plain".into());
-    res.body(format!("{:#?}", HTML_STRUCTURE));
+    let name = ViewVar::Simple(req.query);
+    vars.insert("query".into(), &name);
+
+    res.header("Content-Type".into(), "text/html".into());
+    res.body(render_view(HTML_STRUCTURE, vars));
     res
 }
--- src/views.rs
+++ src/views.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 pub type HtmlView = &'static[HtmlValue];
 
 #[macro_export]
@@ -5,6 +7,22 @@ macro_rules! import_view {
   ($e:expr) => {
     include!(concat!(env!("CARGO_MANIFEST_DIR"), "/", $e, ".in"))
   }
+}
+
+#[derive(Debug)]
+pub enum ViewVar<'a> {
+    Simple(String),
+    Object(&'a HashMap<String, &'a ViewVar<'a>>),
+    Array(Vec<ViewVar<'a>>)
+}
+
+impl<'a> ViewVar<'a> {
+    fn is_object(&self) -> bool {
+        if let ViewVar::Object(_) = self {
+            return true;
+        }
+        false
+    }
 }
 
 #[derive(Debug)]
@@ -17,4 +35,65 @@ pub enum HtmlValue {
 pub enum ViewContent {
     Array(&'static str, &'static str, &'static [HtmlValue]),
     Content(&'static str),
+}
+
+fn get_var_value<'a>(index: &str, vars: &'a HashMap<String, &ViewVar<'a>>) -> Option<&'a ViewVar<'a>>{
+    let mut values = index.split('.').peekable();
+    let mut tmp_vars = &ViewVar::Object(vars);
+
+    while let Some(value) = values.next() {
+        if let ViewVar::Object(vars) = tmp_vars {
+            if let Some(var) = vars.get(value) {
+                if values.peek().is_some() {
+                    if var.is_object() {
+                        tmp_vars = var;
+                    } else {
+                        return None;
+                    }
+                } else {
+                    return Some(var);
+                }
+            } else {
+                return None;
+            }
+        }
+    }
+    None
+}
+
+impl HtmlValue {
+    fn render(&self, vars: &HashMap<String, &ViewVar>) -> Option<String> {
+        match self {
+            HtmlValue::Litteral(value) => Some(value.to_string()),
+            HtmlValue::Value(ViewContent::Content(value)) => {
+                if let Some(ViewVar::Simple(var)) = get_var_value(value, vars) {
+                    Some(var.to_string())
+                } else {
+                    None
+                }
+            }
+            HtmlValue::Value(ViewContent::Array(array, name, childrens)) => {
+                if let Some(ViewVar::Array(array)) = get_var_value(array, vars) {
+                    let mut new_hash = vars.clone();
+                    return Some(array.iter().map(|elem| {
+                        new_hash.insert(name.to_string(), elem);
+                        childrens.iter().map(|elem| if let Some(value) = elem.render(&new_hash) {
+                            value
+                        } else {
+                            String::new()
+                        }).collect::<String>()
+                    }).collect::<String>());
+                }
+                None
+            }
+        }
+    }
+}
+
+pub fn render_view(view: HtmlView, vars: HashMap<String, &ViewVar>) -> String {
+    view.iter().map(|elem| if let Some(value) = elem.render(&vars) {
+            value
+        } else {
+            String::new()
+        }).collect::<String>()
 }
